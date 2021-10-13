f75aca2e21e5a83d7b794de69d13cd7c349baa7d
tfarich <44736976+tfarich@users.noreply.github.com>
2021-10-13 15:15:36 +0300

    Task 1 roughly complete, onto Task 2

--- testing/taylor/tasks1to4attempt.py
+++ testing/taylor/tasks1to4attempt.py
@@ -1,4 +1,19 @@
+from typing import OrderedDict
 import wikipediaapi
+from bs4 import BeautifulSoup
+import requests
+import re
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('stopwords')
 
 ### Initial setup (Task 1) - unprocessed pages, subsections, and list of entities (except references) extracted ###
 
@@ -25,25 +40,128 @@ def setup():
         ('environmentally_friendly', wikipedia.page('environmentally friendly').sections)
     ])
 
-    # TODO: Eliminate references from links
+    # TAYLOR: wikipedia-api doesn't separate the references and is alphabetical, producing a messy list of links
+    # TAYLOR: decided to use beautiful soup as an alternative
+
+    # nature
+    page_request = requests.get("https://en.wikipedia.org/wiki/Nature")
+    beautiful_soup = BeautifulSoup(page_request.text, "html.parser")
+    nature_entities_list = OrderedDict([])
+    for link in beautiful_soup.find_all("a"):
+        url = link.get("href", "")
+        title = link.get("title", "")
+        if title == "Balance of nature":
+            nature_entities_list[title] = url
+            break
+        else:
+            nature_entities_list[title] = url
+    
+    #pollution
+    page_request = requests.get("https://en.wikipedia.org/wiki/Pollution")
+    beautiful_soup = BeautifulSoup(page_request.text, "html.parser")
+    pollution_entities_list = OrderedDict([])
+    for link in beautiful_soup.find_all("a"):
+        url = link.get("href", "")
+        title = link.get("title", "")
+        if url == "http://www.merriam-webster.com/dictionary/pollution":
+            break
+        else:
+            pollution_entities_list[title] = url
+
+    #sustainability
+    page_request = requests.get("https://en.wikipedia.org/wiki/Sustainability")
+    beautiful_soup = BeautifulSoup(page_request.text, "html.parser")
+    sustainability_entities_list = OrderedDict([])
+    for link in beautiful_soup.find_all("a"):
+        url = link.get("href", "")
+        title = link.get("title", "")
+        if title == "Sustainable (song)":
+            sustainability_entities_list[title] = url
+            break
+        else:
+            sustainability_entities_list[title] = url
+
+    #environmentally_friendly
+    page_request = requests.get("https://en.wikipedia.org/wiki/Environmentally_friendly")
+    beautiful_soup = BeautifulSoup(page_request.text, "html.parser")
+    environmentally_friendly_entities_list = OrderedDict([])
+    for link in beautiful_soup.find_all("a"):
+        url = link.get("href", "")
+        title = link.get("title", "")
+        if title == "Sustainable products":
+            environmentally_friendly_entities_list[title] = url
+            break
+        else:
+            environmentally_friendly_entities_list[title] = url
+
     global page_entities_list
     page_entities_list = dict([
-        ('nature', wikipedia.page('Nature').links),
-        ('pollution', wikipedia.page('pollution').links),
-        ('sustainability', wikipedia.page('sustainability').links),
-        ('environmentally_friendly', wikipedia.page('environmentally friendly').links)
+        ('nature', nature_entities_list),
+        ('pollution', pollution_entities_list),
+        ('sustainability', sustainability_entities_list),
+        ('environmentally_friendly', environmentally_friendly_entities_list)
     ])
 
-### Preprocessing and lemmatization (Task 2A) ###
+### Preprocessing, lemmatizing, and combining into a corpus (Task 2A, 3A, 4A) ###
 
-def preprocessing_and_lemmatization():
-    print("preprocessing")
+def corpus_creation(unprocessed_documents):
+    
+    corpus = ""
+    for key in unprocessed_documents:
+        document = unprocessed_documents[key]
 
-### Corpus creation (Task 2B) ###
+        # preprocess
 
-def corpus_creation():
-    print("corpus creation")
+        # to lowercase
+        document = document.lower()
+        # remove symbols/special characters
+        document = re.sub(r'\W', ' ', str(document))
+        # remove single characters
+        document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+        # remove single characters from the first characters
+        document = re.sub(r'\^[a-zA-Z]\s+', ' ', document)
+        # standardize number of spaces >1 space becomes 1 space
+        document = re.sub(r'\s+', ' ', document, flags=re.I)
+        # remove any prefixed "b"
+        document = re.sub(r'^b\s+', '', document)
+
+        # lemmatize
+
+        document_tokens = document.split()
+        document_tokens = [nltk.stem.WordNetLemmatizer().lemmatize(word) for word in document_tokens]
+        document_tokens = [word for word in document_tokens if word not in (set(nltk.corpus.stopwords.words('english') and set(nltk.corpus.stopwords.word('french')))) ]
+        document_tokens = [word for word in document_tokens if len(word) > 2]
+        
+        print(document_tokens)
+        break
+
+    return corpus
+
+### TfldfVectorizer creation (Task 2B, 3B, 4B) ###
+
+def vectorizer():
+    print("tfldfVectorizer")
+
+### Calculate cosine similarity (Task 2C, 3C, 4C) ###
+
+def cosine_similarity():
+    print("cosine similarity")
+
+### main ###
 
 setup()
-preprocessing_and_lemmatization()
-corpus_creation()
+
+# Task 2
+corpus_creation(unprocessed_page)
+#vectorizer()
+#cosine_similarity()
+
+# Task 3
+corpus_creation(page_subsections)
+#vectorizer()
+#cosine_similarity()
+
+# Task 4
+corpus_creation(page_entities_list)
+#vectorizer()
+#cosine_similarity()
