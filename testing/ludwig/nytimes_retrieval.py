0c32de4c7403ae51413412d8d8b0b0bed64f8d72
Ludwig Lohmer <ludwig.lohmer@student.oulu.fi>
2021-10-18 15:31:37 +0300

    Add pre-processing function from other file for testing
    Add function and test a simple case

--- testing/ludwig/nytimes_retrieval.py
+++ testing/ludwig/nytimes_retrieval.py
@@ -2,9 +2,45 @@ from pynytimes import NYTAPI
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import datetime
+import nltk
+import re
 
 
 # docs: https://pypi.org/project/pynytimes/#article-search
+
+
+# functions
+
+# just copied from: https://stackabuse.com/python-for-nlp-working-with-facebook-fasttext-library/
+# observation: year numbers and citation codes remain after preprocessing
+def preprocess_text(document):
+    # Remove all the special characters
+    document = re.sub(r'\W', ' ', str(document))
+
+    # remove all single characters
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+
+    # Remove single characters from the start
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document)
+
+    # Substituting multiple spaces with single space
+    document = re.sub(r'\s+', ' ', document, flags=re.I)
+
+    # Removing prefixed 'b'
+    document = re.sub(r'^b\s+', '', document)
+
+    # Converting to Lowercase
+    document = document.lower()
+
+    # Lemmatization
+    tokens = document.split()
+    tokens = [nltk.stem.WordNetLemmatizer().lemmatize(word) for word in tokens]
+    tokens = [word for word in tokens if word not in set(nltk.corpus.stopwords.words('english'))]
+    tokens = [word for word in tokens if len(word) > 3]
+
+    preprocessed_text = ' '.join(tokens)
+
+    return preprocessed_text
 
 
 # definitions
@@ -38,6 +74,10 @@ for i, article in enumerate(articles):
 
 # check number of articles
 print('===\nWe have', len(articles), 'articles.\n===')
+
+
+# pre-process the documents
+list_of_articles = preprocess_text(list_of_articles)
 
 # create wordcloud and plot it
 whole_str = ' '.join(list_of_articles) # join all the strings in the list to one string
