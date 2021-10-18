b7afe79fe2901e03c871e9a9784a598b209f5158
Ludwig Lohmer <ludwig.lohmer@student.oulu.fi>
2021-10-18 15:30:39 +0300

    Fix small type
    indexing is wrong here; use whole list

--- testing/ludwig/nytimes_retrieval.py
+++ testing/ludwig/nytimes_retrieval.py
@@ -40,7 +40,7 @@ for i, article in enumerate(articles):
 print('===\nWe have', len(articles), 'articles.\n===')
 
 # create wordcloud and plot it
-whole_str = ' '.join(list_of_articles[i]) # join all the strings in the list to one string
+whole_str = ' '.join(list_of_articles) # join all the strings in the list to one string
 wordcloud = WordCloud().generate(whole_str)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.title('Example word cloud')
