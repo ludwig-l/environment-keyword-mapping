0802e18a048b5384a7a8505e928c5f364eaed259
Ludwig Lohmer <ludwig.lohmer@student.oulu.fi>
2021-10-18 16:06:37 +0300

    Fix word cloud bug only showing single letters in this version

--- testing/ludwig/nytimes_retrieval.py
+++ testing/ludwig/nytimes_retrieval.py
@@ -80,8 +80,7 @@ print('===\nWe have', len(articles), 'articles.\n===')
 list_of_articles = preprocess_text(list_of_articles)
 
 # create wordcloud and plot it
-whole_str = ' '.join(list_of_articles) # join all the strings in the list to one string
-wordcloud = WordCloud().generate(whole_str)
+wordcloud = WordCloud().generate(list_of_articles)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.title('Example word cloud')
 plt.axis('off')
