7f980eeddd87b26174ebc29b11987aead1e5059a
Ludwig Lohmer <ludwig.lohmer@student.oulu.fi>
2021-10-19 17:57:52 +0300

    Begin with the news forum retrieval script
    Add some code and notes for the approach

--- news_forum_retrieval.py
+++ news_forum_retrieval.py
@@ -1,5 +1,8 @@
 # news forum retrieval
 
+import datetime
+from pynytimes import NYTAPI
+
 """
 The plan:
 
@@ -10,3 +13,47 @@ The plan:
 -   create a wordcloud for each document
 -   calculate the TfIdf for all pairs for the time periods for each keyword
 """
+
+
+# keywords
+from datetime import datetime
+
+
+keywords = [
+    'nature', 'pollution', 'sustainability', 'environmentally friendly'
+]
+
+# time periods
+time_periods = [
+    (datetime.datetime(2000, 1, 1), datetime.datetime(2000, 12, 31)),
+    (datetime.datetime(2010, 1, 1), datetime.datetime(2010, 12, 31)),
+    (datetime.datetime(2020, 1, 1), datetime.datetime(2020, 12, 31)),
+]
+
+# definitions for the news forum API
+api_key = ''
+with open('nytimes_api_key.txt', 'r') as file:
+    api_key = file.read()
+nyt = NYTAPI(key=api_key, parse_dates=True)
+n_articles = 100 # number of articles to retrieve with each API call
+
+# retrieve the articles
+# articles = [[], []]
+for i, keyword in enumerate(keywords):
+    for j, time_period in enumerate(time_periods):
+
+    article = nyt.article_search(
+        query = keyword,
+        results = n_articles,
+        dates = {
+            'begin': time_period[0],
+            'end': time_period[1]
+        },
+        options = {
+            'sort': 'relevance',
+            'sources': 'New York Times'
+        }
+    )
+
+    # at this point the article has to be filtered out of all the different data and saved
+    # in a proper data structure (use maybe a nested list or a dict here?)
