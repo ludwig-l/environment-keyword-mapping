87e52dfb09f888627c1f2c0ddfb3d1371095a03c
Ludwig Lohmer <ludwig.lohmer@student.oulu.fi>
2021-10-19 17:32:43 +0300

    Add new file for news forum retrieval tasks (9 and 10)
    with plan for the script

---
+++ news_forum_retrieval.py
@@ -0,0 +1,12 @@
+# news forum retrieval
+
+"""
+The plan:
+
+-   define the four keywords
+-   define the time periods of interest
+-   retrieve the articles for each keyword and time period
+-   pre-process all the documents
+-   create a wordcloud for each document
+-   calculate the TfIdf for all pairs for the time periods for each keyword
+"""
