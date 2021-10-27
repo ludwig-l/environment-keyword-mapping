e53179e9ec810ee1c373e8c283ae70acf9fe987a
Ludwig Lohmer <ludwig.lohmer@student.oulu.fi>
2021-10-27 12:55:58 +0300

    Add a scrollable text box and try text feature
    Button now triggers text box to show some text.

--- gui_app.py
+++ gui_app.py
@@ -1,6 +1,7 @@
 # this will be the GUI application (hopefully ...)
 
 import tkinter as tk
+from tkinter import scrolledtext
 
 
 # some definitions
@@ -14,6 +15,17 @@ window.geometry('600x800')
 # window.grid_rowconfigure(0, weight=1)
 # window.grid_columnconfigure(0, weight=1)
 
+
+# functions to execute actions
+
+def button_clicked():
+    # write text to the button; need to disable and enable afterwards
+    txt_t1.configure(state='normal')
+    txt_t1.delete('1.0', tk.END)
+    txt_t1.insert(tk.END, '42')
+    txt_t1.configure(state='disabled')
+
+
 # set up labels
 
 # task 1
@@ -21,13 +33,12 @@ label_t1 = tk.Label(window, text='First task', font=default_label_font)
 label_t1.grid(row=0, column=0)
 label_t1.grid_rowconfigure(1, weight=1)
 label_t1.grid_columnconfigure(1, weight=1)
-btn_t1 = tk.Button(window, text=default_button_text)
-def clicked():
-    btn_t1.configure(text='42')
+btn_t1 = tk.Button(window, text=default_button_text, command=button_clicked)
 btn_t1.grid(row=0, column=1)
 #btn_t1.grid_rownconfigure(1, weight=1)
 #btn_t1.grid_columnconfigure(1, weight=1)
-txt_t1 = tk.Entry(window)
+txt_t1 = scrolledtext.ScrolledText(master=window, wrap=tk.WORD)
+txt_t1.configure()
 txt_t1.grid(row=1, column=0)
 
 # task 2
@@ -53,6 +64,5 @@ label_t4.grid_rowconfigure(1)
 label_t4.grid_columnconfigure(1, weight=1)
 btn_t4 = tk.Button(window, text=default_button_text)
 btn_t4.grid(row=4, column=1)
-
 
 window.mainloop()
