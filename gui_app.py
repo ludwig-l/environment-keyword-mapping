dc0806a68f7667025e89ee4d0c70abccce379176
Ludwig Lohmer <ludwig.lohmer@student.oulu.fi>
2021-10-27 13:25:51 +0300

    Add texts for the different tasks as list
    Will be used for label texts.

--- gui_app.py
+++ gui_app.py
@@ -7,6 +7,18 @@ from tkinter import scrolledtext
 # some definitions
 default_button_text = 'Compute'
 default_label_font = ('Arial Bold', 20)
+text_tasks = [
+    '1. Show Wikipedia pages for each keyword.',
+    '2. Pre-process each dosument and calculate the cosine similarity measure of each document pair via Tf-Idf vectorizer approach.',
+    '3. Show all titles of subsections of the Wikipedia pages and repeat the similarity score calculation.',
+    '4. Show entity categories of the Wikipedia pages and repeat the similarity score calculation.',
+    '5. Calculate Wu and Palmer semantic similarity between each keyword pari and calculate the correlation between the sementic similarity result and each of the three Wikipedia based similarities.',
+    '6. Scrap content of each entity and retrieve all the entity-categories identified during this first exploration stage (first pass exploration).',
+    '7. Repeat Tf-Idf vectorizer representation and recalculate the cosine similarity between individual words of the four keywords.',
+    '8. Use a pre-trained word2vec model to represent each words and calculate the corresponding similarity.',
+    '9. Retrieve articles from a news forum and retrieve information for different time periods. Display the word cloud representation for each document.',
+    '10. Repeat the Tf-Idf based similarity calculus amoung each pair at each time.',
+]
 
 # set up screen
 window = tk.Tk()
@@ -29,7 +41,7 @@ def button_clicked():
 # set up labels
 
 # task 1
-label_t1 = tk.Label(window, text='First task', font=default_label_font)
+label_t1 = tk.Label(window, text=text_tasks[0], font=default_label_font)
 label_t1.grid(row=0, column=0)
 label_t1.grid_rowconfigure(1, weight=1)
 label_t1.grid_columnconfigure(1, weight=1)
@@ -42,7 +54,9 @@ txt_t1.configure()
 txt_t1.grid(row=1, column=0)
 
 # task 2
-label_t2 = tk.Label(window, text='Second task', font=default_label_font)
+label_t2 = tk.Label(window,
+                    text=text_tasks[1],
+                    font=default_label_font)
 label_t2.grid(row=2, column=0)
 label_t2.grid_rowconfigure(1, weight=1)
 label_t2.grid_columnconfigure(1, weight=1)
@@ -50,7 +64,9 @@ btn_t2 = tk.Button(window, text=default_button_text)
 btn_t2.grid(row=2, column=1)
 
 # task 3
-label_t3 = tk.Label(window, text='Third task', font=default_label_font)
+label_t3 = tk.Label(window,
+                    text=text_tasks[2],
+                    font=default_label_font)
 label_t3.grid(row=3, column=0)
 label_t3.grid_rowconfigure(1, weight=1)
 label_t3.grid_columnconfigure(1, weight=1)
@@ -58,7 +74,7 @@ btn_t3 = tk.Button(window, text=default_button_text)
 btn_t3.grid(row=3, column=1)
 
 # task 4
-label_t4 = tk.Label(window,text='Fourth task', font=default_label_font)
+label_t4 = tk.Label(window,text=text_tasks[3], font=default_label_font)
 label_t4.grid(row=4, column=0)
 label_t4.grid_rowconfigure(1)
 label_t4.grid_columnconfigure(1, weight=1)
