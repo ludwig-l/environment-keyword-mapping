8df45b8edcf20769078ce5a76b9f4aed2e91b285
Ludwig Lohmer <ludwig.lohmer@student.oulu.fi>
2021-10-31 16:38:57 +0200

    Add class template
    Adds a new class for storing the important functions and variables needed for the tasks. Currently, just an empty sample class basically.

---
+++ utils.py
@@ -0,0 +1,9 @@
+# class for providing all necessary functions and variables for storing the data of interest
+
+class Utils:
+    def __init__(self):
+        # something something ...
+        tmp = 42 
+    main_data_struct = {}
+
+    # very important functions/variables will follow here ...
